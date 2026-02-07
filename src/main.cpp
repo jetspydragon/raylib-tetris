46e6ee044db7792cbdcc25cd0ee0cd85d533f9fe
Eduardo Ahumada <eduardoahumada.cactus@gmail.com>
2026-02-07 16:33:47 -0300

    Code adjustment for web execution

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,45 @@
 
 #include "game.h"
 
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+void UpdateDrawFrame(void);
+
+Color darkBlue = {44, 44, 127, 255};
+Game game = Game();
+
 int main(void)
-{
-    Color darkBlue = {44, 44, 127, 255};
+{    
     const int screenWidth = 300;
     const int screenHeight = 600;
     InitWindow(screenWidth, screenHeight, "raylib tetris");
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else    
     SetTargetFPS(60);
 
-    Game game = Game();
     while(!WindowShouldClose())
     {
-        game.handleInput();
-        
-        BeginDrawing();
-        ClearBackground(darkBlue);
-
-        game.draw();
-
-        EndDrawing();
+        UpdateDrawFrame();
     }
+#endif    
 
     CloseWindow();
     
     return 0;
+}
+
+void UpdateDrawFrame(void)
+{
+    game.handleInput();
+
+    BeginDrawing();
+    ClearBackground(darkBlue);
+
+    game.draw();
+
+    EndDrawing();
 }
